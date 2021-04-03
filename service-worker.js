f16603bcf02005fc47a036a1fded73b866cc5cc2
klevain <klevain@protonmail.ch>
2021-04-03 13:44:06 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-var cacheName = 'hello-pwa';
+var cacheName = 'basic-pwa';
 var filesToCache = [
   '/',
   '/index.html',
@@ -7,8 +7,8 @@ var filesToCache = [
 ];
 
 /* Start the service worker and cache all of the app's content */
-self.addEventListener('install', function(e) {
-  e.waitUntil(
+self.addEventListener('install', function(event) {
+  event.waitUntil(
     caches.open(cacheName).then(function(cache) {
       return cache.addAll(filesToCache);
     })
@@ -16,10 +16,10 @@ self.addEventListener('install', function(e) {
 });
 
 /* Serve cached content when offline */
-self.addEventListener('fetch', function(e) {
-  e.respondWith(
-    caches.match(e.request).then(function(response) {
-      return response || fetch(e.request);
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
     })
   );
 });
